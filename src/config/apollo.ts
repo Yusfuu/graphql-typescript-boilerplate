f16497ceb0283af9c70268542771328eb5967825
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-02-27 13:19:05 +0100

    fix apollo gql playground

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -7,12 +7,19 @@ import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import { context } from './context';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const startApolloServer = async (schema: any) => {
   const port = process.env.PORT || 4000;
 
   const app = express();
   app.use(compression());
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: isProduction,
+      crossOriginEmbedderPolicy: isProduction,
+    })
+  );
 
   const httpServer = http.createServer(app);
 
